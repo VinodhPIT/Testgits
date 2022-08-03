318a93d8688bb5edd0f7767e982af6514aec2971
Vinodh <vinodh.mn@pitsolutions.com>
2022-08-03 11:09:28 +0530

    okda going

---
+++ lib/Screens/Pushnotificatin.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Push extends StatefulWidget {
+  const Push({Key? key}) : super(key: key);
+
+  @override
+  State<Push> createState() => _PushState();
+}
+
+class _PushState extends State<Push> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
